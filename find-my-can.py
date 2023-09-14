9dbbfb7906c5b252a6c87dfaf8e36ec9f0a84e0c
Alexandre Machado <alexandre@machado.cc>
2023-09-13 23:32:37 -0300

    iterate and find

--- find-my-can.py
+++ find-my-can.py
@@ -1,6 +1,7 @@
 import ipaddress
 import socket
 import multiprocessing
+import re
 import time
 from random import random
 from tqdm import tqdm
@@ -12,12 +13,12 @@ def isReachable(ipOrName, port, timeout=1):
     try:
         s.connect((ipOrName, int(port)))
         s.shutdown(socket.SHUT_RDWR)
-        print(ipOrName + ": ✅ online")
-        # return ipOrName + ": ✅ online"
+        # print(ipOrName + ": ✅ online")
+        return ipOrName + ": ✅ online"
         # pool.terminate()
     except:
-        print(ipOrName + ": ⛔ offline")
-    # return ipOrName + ": ⛔ offline"
+        # print(ipOrName + ": ⛔ offline")
+        return ipOrName + ": ⛔ offline"
     finally:
         s.close()
 
@@ -32,17 +33,24 @@ def task(arg):
 
 def main():
     # entry point for the program
-    items = ipaddress.IPv4Network("192.168.0.0/24")
+    items = ipaddress.IPv4Network("192.168.0.0/22")
     print(
         f"my ip: {socket.gethostbyname(socket.gethostname())} itens: {items.num_addresses}"
     )
 
     # create the process pool
     pool = multiprocessing.Pool(60)
-    tqdm(pool.imap(task, items), total=items.num_addresses)
+    result_list = []
+    for result in tqdm(pool.imap(func=task, iterable=items), total=items.num_addresses):
+        result_list.append(result)
+
+    print("found ips:")
+    print([x for x in result_list if re.match(".*online.*", x)])
+
+    # print(result_list.index("192.168.0.228: ⛔ offline"))
     # pool.imap(task, items)
-    pool.close()
-    pool.join()
+    # pool.close()
+    # pool.join()
 
 
 if __name__ == "__main__":

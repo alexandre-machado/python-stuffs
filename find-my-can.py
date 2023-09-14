6515c058713e18d75ebf62db29a5e1e889636a84
Alexandre Machado <alexandre@machado.cc>
2023-09-13 22:11:48 -0300

    samples

--- find-my-can.py
+++ find-my-can.py
@@ -1,16 +1,12 @@
 import ipaddress
 import socket
 import multiprocessing
-from time import sleep
+import time
 from random import random
+from tqdm import tqdm
 
 
-class WorkerStopException(Exception):
-    pass
-
-
-def isReachable(ipOrName, port, timeout=2):
-    # global pool
+def isReachable(ipOrName, port, timeout=1):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(timeout)
     try:
@@ -21,7 +17,7 @@ def isReachable(ipOrName, port, timeout=2):
         # pool.terminate()
     except:
         print(ipOrName + ": ⛔ offline")
-        # return ipOrName + ": ⛔ offline"
+    # return ipOrName + ": ⛔ offline"
     finally:
         s.close()
 
@@ -29,17 +25,25 @@ def isReachable(ipOrName, port, timeout=2):
 # task to execute in another process
 def task(arg):
     return isReachable(str(arg), 554)
+    # time.sleep(0.5)
+
+    # return str(arg)
 
 
-# entry point for the program
-if __name__ == "__main__":
-    # global pool
+def main():
+    # entry point for the program
     items = ipaddress.IPv4Network("192.168.0.0/24")
     print(
         f"my ip: {socket.gethostbyname(socket.gethostname())} itens: {items.num_addresses}"
     )
+
     # create the process pool
     pool = multiprocessing.Pool(60)
-    pool.imap(task, items)
+    tqdm(pool.imap(task, items), total=items.num_addresses)
+    # pool.imap(task, items)
     pool.close()
     pool.join()
+
+
+if __name__ == "__main__":
+    main()
